6c835fcda5412b7a9833a12e7e9637a3f9e6de33
Murat-Gencer <mrt.ebr.kbr@hotmail.com>
2026-02-01 16:54:19 +0300

    update codes for build

--- src/pages/ConfigEditor.js
+++ src/pages/ConfigEditor.js
@@ -17,7 +17,7 @@ const ConfigEditor = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-  const user = apiService.auth.getCurrentUser();
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [editingConfig, setEditingConfig] = useState(null);
 
@@ -69,11 +69,6 @@ const ConfigEditor = () => {
       }
     }
   );
-
-  const handleLogout = () => {
-    apiService.auth.logout();
-    navigate('/');
-  };
 
   const handleEdit = (config) => {
     setEditingConfig(config);
